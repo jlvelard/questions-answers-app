a5082b30727a954f79df010c0f1cd49bb5a97e55
Jose Velarde Kuncar <jlvelard@uark.edu>
2017-10-26 15:13:38 -0500

    GitTest

---
+++ GitTest/GitTest/Form1.vb
@@ -0,0 +1,60 @@
+﻿Public Class db
+    ' login to database server
+    Protected connection As New SqlConnection With {.ConnectionString = "Server=essql1.walton.uark.edu;Database=isys4283-2017fa;Trusted_Connection=yes;"}
+    ' prepare a query
+    Protected command As New SqlCommand With {.connection = connection}
+
+    ' set and get sql command
+    Public Property sql() As String
+        Get
+            Return command.CommandText
+        End Get
+
+        Set(value As String)
+            command.CommandText = value
+        End Set
+    End Property
+
+    ' populate a data grid view
+    Public Sub fill(ByRef dgv As DataGridView)
+        Dim adapter As SqlDataAdapter
+        Dim dataset As DataSet
+
+        Try
+            connection.Open()
+            adapter = New SqlDataAdapter(command)
+            dataset = New DataSet
+            adapter.Fill(dataset)
+            If dataset.Tables.Count > 0 Then
+                dgv.Refresh()
+                dgv.DataSource = dataset.Tables(0)
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            Throw ex
+        Finally
+            If connection.State = ConnectionState.Open Then
+                connection.Close()
+            End If
+        End Try
+    End Sub
+
+
+    ' execute dml statement
+    Public Sub execute()
+
+        Try
+            connection.Open()
+            command.ExecuteNonQuery()
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            Throw ex
+        Finally
+            If connection.State = ConnectionState.Open Then
+                connection.Close()
+            End If
+        End Try
+    End Sub
+End Class
+
